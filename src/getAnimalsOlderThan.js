4b7df37f1f5ad6bfa42e6064db3e503951015153
Tiago Oliveira <pliveiratiago7890@gmail.com>
2022-07-31 18:55:40 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const idadeMinima = data.species.find((element) => element.name === animal);
+  return idadeMinima.residents.every((resident) => resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
