7f3edf1772d4c57f57ffbecc7feb8ede098d8d7d
Tiago Oliveira <pliveiratiago7890@gmail.com>
2022-08-01 17:16:34 -0300

    Ajustando lint getOpeningHours 3

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -4,25 +4,24 @@ describe('Testes da função getOpeningHours', () => {
   it('Teste não passando argumentos. Deverá retornar o objeto', () => {
     const actual = getOpeningHours();
     const expected = {
-    Tuesday: { open: 8, close: 6 },
-    Wednesday: { open: 8, close: 6 },
-    Thursday: { open: 10, close: 8 },
-    Friday: { open: 10, close: 8 },
-    Saturday: { open: 8, close: 10 },
-    Sunday: { open: 8, close: 8 },
-    Monday: { open: 0, close: 0 },
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
     };
-    expect(actual).toEqual(expected);
-  });
+    expect(actual).toEqual(expected); });
   it('Para os argumentos Monday e 09:00-AM deve retornar a string The zoo is closed', () => {
-    const actual = getOpeningHours('Monday', '09:00-AM');
-    const expected = 'The zoo is closed';
-    expect(actual).toEqual(expected);
+        const actual = getOpeningHours('Monday', '09:00-AM');
+        const expected = 'The zoo is closed';
+        expect(actual).toEqual(expected);
     });
-  it('Para os argumentos Tuesday e 09:00-AM deve retornar a string The zoo is open', () => {
-    const actual = getOpeningHours('Tuesday', '09:00-AM');
-    const expected = 'The zoo is open';
-    expect(actual).toEqual(expected);
+    it('Para os argumentos Tuesday e 09:00-AM deve retornar a string The zoo is open', () => {
+        const actual = getOpeningHours('Tuesday', '09:00-AM');
+        const expected = 'The zoo is open';
+        expect(actual).toEqual(expected);
     });
     it('Para os argumentos Wednesday e 09:00-PM deve retornar a string The zoo is closed', () => {
         const actual = getOpeningHours('Wednesday', '09:00-PM');
