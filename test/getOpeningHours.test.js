ac4d1dd6365690ccb8bcb94b4dbcdca66e966a55
Tiago Oliveira <pliveiratiago7890@gmail.com>
2022-08-01 16:57:48 -0300

    Ajustando lint getOpeningHours

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -2,16 +2,16 @@ const getOpeningHours = require('../src/getOpeningHours');
 
 describe('Testes da função getOpeningHours', () => {
     it('Teste não passando argumentos. Deverá retornar o objeto', () => {
-        const actual = getOpeningHours();
-        const expected = {
-            Tuesday: { open: 8, close: 6 },
-            Wednesday: { open: 8, close: 6 },
-            Thursday: { open: 10, close: 8 },
-            Friday: { open: 10, close: 8 },
-            Saturday: { open: 8, close: 10 },
-            Sunday: { open: 8, close: 8 },
-            Monday: { open: 0, close: 0 },
-        };
+      const actual = getOpeningHours();
+      const expected = {
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    };
         expect(actual).toEqual(expected);
     });
     it('Para os argumentos Monday e 09:00-AM deve retornar a string The zoo is closed', () => {
