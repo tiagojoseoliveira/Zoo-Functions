5d566ab338ff06d6bd9891daa9aaacf31c8b8ac8
Tiago Oliveira <pliveiratiago7890@gmail.com>
2022-08-01 17:26:25 -0300

    Ajustando lint getOpeningHours 5

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -13,40 +13,40 @@ describe('Testes da função getOpeningHours', () => {
       Monday: { open: 0, close: 0 },
     };
     expect(actual).toEqual(expected);
-});
+  });
   it('Para os argumentos Monday e 09:00-AM deve retornar a string The zoo is closed', () => {
-        const actual = getOpeningHours('Monday', '09:00-AM');
-        const expected = 'The zoo is closed';
-        expect(actual).toEqual(expected);
-    });
+    const actual = getOpeningHours('Monday', '09:00-AM');
+    const expected = 'The zoo is closed';
+    expect(actual).toEqual(expected);
+  });
   it('Para os argumentos Tuesday e 09:00-AM deve retornar a string The zoo is open', () => {
-        const actual = getOpeningHours('Tuesday', '09:00-AM');
-        const expected = 'The zoo is open';
-        expect(actual).toEqual(expected);
+    const actual = getOpeningHours('Tuesday', '09:00-AM');
+    const expected = 'The zoo is open';
+    expect(actual).toEqual(expected);
     });
   it('Para os argumentos Wednesday e 09:00-PM deve retornar a string The zoo is closed', () => {
-        const actual = getOpeningHours('Wednesday', '09:00-PM');
-        const expected = 'The zoo is closed';
-        expect(actual).toEqual(expected);
+    const actual = getOpeningHours('Wednesday', '09:00-PM');
+    const expected = 'The zoo is closed';
+    expect(actual).toEqual(expected);
     });
   it('Vericar case sentive retorna The zoo is open', () => {
-        const actual = getOpeningHours('Thursday', '10:00-am');
-        const expected = 'The zoo is open';
-        expect(actual).toEqual(expected);
+    const actual = getOpeningHours('Thursday', '10:00-am');
+    const expected = 'The zoo is open';
+    expect(actual).toEqual(expected);
     });
   it('Vericar se o erro é lançado quando parametro diferente de numero', () => {
-        expect(() => { getOpeningHours('Thursday', 'nove-am'); }).toThrow();
+    expect(() => { getOpeningHours('Thursday', 'nove-am'); }).toThrow();
     });
   it('Vericar se o erro é lançado final horario diferente de am ou pm', () => {
-        expect(() => { getOpeningHours('Thursday', '10:00-ap'); }).toThrow();
+    expect(() => { getOpeningHours('Thursday', '10:00-ap'); }).toThrow();
     });
   it('The hour must be between 0 and 12', () => {
-        expect(() => { getOpeningHours('Thursday', '14:00-am'); }).toThrow();
+    expect(() => { getOpeningHours('Thursday', '14:00-am'); }).toThrow();
     });
   it('The minutes must be between 0 and 59', () => {
-        expect(() => { getOpeningHours('Thursday', '10:100-am'); }).toThrow();
+    expect(() => { getOpeningHours('Thursday', '10:100-am'); }).toThrow();
     });
   it('Não é dia de semana', () => {
-        expect(() => { getOpeningHours('Tiago', '10:00-am'); }).toThrow();
+    expect(() => { getOpeningHours('Tiago', '10:00-am'); }).toThrow();
     });
 });
