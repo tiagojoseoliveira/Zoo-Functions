03276916549c8c34555e2ebf25ba74f9393cd27c
Tiago Oliveira <pliveiratiago7890@gmail.com>
2022-08-02 14:12:36 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  // species
+  // const rebeldes = pilots.filter(pilot => pilot.faction === "Rebels")
+  // return species.filter((animal) => animal.id === ids);
+  // const idadeMinima = data.species.find((element) => element.name === animal);
+  // return idadeMinima.residents.every((resident) => resident.age >= age);
+  if (ids.length < 1) { return []; }
+  return data.species.filter((element) => element.id === ids[0] || element.id === ids[1]);
 }
-
 module.exports = getSpeciesByIds;
