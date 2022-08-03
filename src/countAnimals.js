d08af800fa199c146dd3bd2a45f5732b2da791fe
Tiago Oliveira <pliveiratiago7890@gmail.com>
2022-08-03 10:13:42 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,14 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  const novoObjeto = {};
+  if (typeof animal === 'undefined') {
+    data.species.forEach((a) => { novoObjeto[a.name] = a.residents.length; });
+    return novoObjeto;
+  }
+  const espAnimal = data.species.find((i) => i.name === animal.specie);
+  if (typeof animal.sex === 'undefined') {
+    return espAnimal.residents.length;
+  } return espAnimal.residents.reduce((a, c) => (c.sex === animal.sex ? a + 1 : a), 0);
 }
-
 module.exports = countAnimals;
