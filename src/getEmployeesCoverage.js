164ac7a1403e60f2094d29dfbdb66b592b10f599
Tiago Oliveira <pliveiratiago7890@gmail.com>
2022-08-06 15:01:53 -0300

    Requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,49 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function validandoInfo(object) {
+  const idArr = data.employees.map((element) => element.id);
+  const firstNameArr = data.employees.map((element) => element.firstName);
+  const lastNameArr = data.employees.map((element) => element.lastName);
+  const fullNameArr = [...firstNameArr, ...lastNameArr];
+  if (Object.keys(object)[0] === 'name') {
+    if (fullNameArr.includes(Object.values(object)[0])) {
+      return null;
+    }
+    throw new Error('Informações inválidas');
+  }
+  if (idArr.includes(Object.values(object)[0])) {
+    return null;
+  }
+  throw new Error('Informações inválidas');
 }
 
+function getEmployeeByName(object) {
+  const name = Object.values(object)[0];
+  const person = data.employees.find((objetoE) => objetoE.firstName === name
+    || objetoE.lastName === name);
+  const animais = person.responsibleFor;
+  return {
+    id: person.id,
+    fullName: `${person.firstName} ${person.lastName}`,
+    species: animais.map((idAnimais) => data.species.find((bixo) => bixo.id === idAnimais).name),
+    locations: animais.map((idAnimais) => data.species.find((bixo) => bixo.id === idAnimais)
+    .location),
+  };
+}
+
+function getEmployeesCoverage(object) {
+  // seu código aqui]
+  if (!(object)) {
+    return data.employees.map((employee) => getEmployeeByName({ name: employee.firstName }));
+  }
+  validandoInfo(object);
+  if (Object.keys(object)[0] === 'name') {
+    return getEmployeeByName(object);
+  }
+  if (Object.keys(object)[0] === 'id') {
+    const objeto = Object.values(object)[0];
+    const name = data.employees.find((objetoE) => objetoE.id === objeto).firstName;
+    return getEmployeeByName({ name });
+  }
+}
 module.exports = getEmployeesCoverage;
